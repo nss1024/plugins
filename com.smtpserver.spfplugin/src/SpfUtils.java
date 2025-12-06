9e3ac8adb1941f6d1cf1c540e31a7b27c67c8b6f
nss1024 <32052333+nss1024@users.noreply.github.com>
2025-12-06 12:29:49 +0000

    added support for checking ipv6 ranges

--- com.smtpserver.spfplugin/src/SpfUtils.java
+++ com.smtpserver.spfplugin/src/SpfUtils.java
@@ -1,5 +1,9 @@
 import spf_resolver.SpfReult;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class SpfUtils {
 
         private SpfUtils(){}
@@ -28,6 +32,42 @@ public class SpfUtils {
         int spfIp = ipToInt(parts[0]);
         int mask = prefixToMask(Integer.parseInt(parts[1]));
         return (hostIp&mask)==(spfIp&mask);
+    }
+
+    public static boolean matchesIpv6Cidr(String hostIp, String spfIpRange){
+        InetAddress hostAddress, spfAddress = null;
+        String[] parts = spfIpRange.split("/");
+        String spfPrefix = parts[1];
+        try {
+            hostAddress = InetAddress.getByName(hostIp);
+            spfAddress = InetAddress.getByName(parts[0]);
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        BigInteger hostIpNo = ip6ToBigint(hostAddress);
+        BigInteger spfAddressNo = ip6ToBigint(spfAddress);
+        BigInteger prefix = prefixToBigInt(spfPrefix);
+
+        return hostIpNo.and(prefix).equals(spfAddressNo.and(prefix));
+    }
+
+    private static BigInteger ip6ToBigint(InetAddress addr){
+        return new BigInteger(1,addr.getAddress());
+    }
+
+    private static BigInteger prefixToBigInt(String prefix){
+        int pref = Integer.parseInt(prefix);
+
+        if (pref == 0) {
+            return BigInteger.ZERO;
+        }
+
+        // (1 << pref) - 1
+        BigInteger ones = BigInteger.ONE.shiftLeft(pref).subtract(BigInteger.ONE);
+
+        // Shift into position: fill high bits, zero low bits
+        return ones.shiftLeft(128 - pref);
     }
 
 }
