720f5e20e6d15c77f0af7990886ece577941bb19
nss1024 <32052333+nss1024@users.noreply.github.com>
2026-01-12 18:34:04 +0000

    Added command register

---
+++ com.smtpserver.spfplugin/src/spf_resolver/spf_commands/SpfCommandsRegister.java
@@ -0,0 +1,37 @@
+package spf_resolver.spf_commands;
+
+import javax.activation.CommandMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpfCommandsRegister {
+
+    Map<String, SpfCommand> commandsMap = new HashMap<>();
+
+    SpfCommandsRegister(){
+        loadCommands();
+    }
+
+    public SpfCommand getCommand(String command){
+        if(commandsMap!=null && !commandsMap.isEmpty()){
+            return commandsMap.get(command.toUpperCase());
+        }else{
+            loadCommands();
+            return commandsMap.get(command.toUpperCase());
+        }
+    }
+
+    private void loadCommands(){
+        commandsMap.put("AAAA",new AAAACommand());
+        commandsMap.put("A",new ACommand());
+        commandsMap.put("ALL",new AllCommand());
+        commandsMap.put("EXISTS",new ExistsCommand());
+        commandsMap.put("INCLUDES",new IncludesCommand());
+        commandsMap.put("IP4",new Ip4Command());
+        commandsMap.put("IP6",new Ip6Command());
+        commandsMap.put("MX",new MxCommand());
+        commandsMap.put("PTR",new PtrCommand());
+
+    }
+
+}
