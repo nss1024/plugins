a7b11eb00a2199102a26eecad5001283757324b6
nss1024 <32052333+nss1024@users.noreply.github.com>
2026-01-21 06:55:21 +0000

    Implemented SpfResult to SecurityResult mapping which provides the main lookup return. This result should be passed back to an SMTP server for decision making on how to further ptocess the inbound mail

--- com.smtpserver.spfplugin/src/SpfCheck.java
+++ com.smtpserver.spfplugin/src/SpfCheck.java
@@ -2,7 +2,10 @@ import smtpSecurityApi.SecurityContext;
 import smtpSecurityApi.SecurityPlugin;
 import smtpSecurityApi.SecurityResult;
 import spf_resolver.Lookup;
-import spf_resolver.spf_commands.SpfCommandsRegister;
+import spf_resolver.SpfResult;
+
+
+import java.util.concurrent.ExecutionException;
 
 public class SpfCheck implements SecurityPlugin {
 
@@ -13,7 +16,29 @@ public class SpfCheck implements SecurityPlugin {
         String senderIp = securityContext.get("sender-ip").toString();
         Lookup lookup = new Lookup();
         lookup.start();
-        lookup.lookupSpfRecord(domain,senderIp);
-        return null;
+        SpfResult result;
+        try {
+            result = lookup.lookupSpfRecord(domain,senderIp).get();
+        } catch (InterruptedException | ExecutionException e) {
+           //TODO add log + appropriate return
+            result=SpfResult.TEMPERROR;
+        }
+        return mapSpfToSecurity(result);
     }
+
+    public SecurityResult mapSpfToSecurity(SpfResult spf) {
+         switch (spf) {
+            case PASS : return SecurityResult.ALLOW;
+             case FAIL :
+             case PERMERROR :
+                 return SecurityResult.DENY;
+             case SOFTFAIL : return SecurityResult.QUARANTINE;
+             case NEUTRAL :
+             case NONE :
+                 return SecurityResult.RULE_MATCHED;
+             case TEMPERROR : return SecurityResult.TEMPORARY_REJECT;
+             default: return SecurityResult.QUARANTINE;
+         }
+    }
+
 }
